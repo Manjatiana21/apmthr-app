f110da1673e15c2803a15258abd33ebec223668d
Manjatiana21 <andrianiainamanjatiana719@gmail.com>
2026-05-08 16:07:19 +0300

    Utilisation de REACT_APP_API_URL dans api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,11 +4,10 @@ import axios from "axios";
 
 // ✅ Instance axios avec configuration de base
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000", // backend Django
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: { "Content-Type": "application/json" },
 });
+
 
 // =======================
 // 📌 Refresh Token
