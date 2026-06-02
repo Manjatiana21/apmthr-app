6b7a261aadfa9fa37174ae8d7c079b506069f041
Manjatiana21 <andrianiainamanjatiana719@gmail.com>
2026-06-02 12:25:03 +0300

    Finalisation du projet

--- app_apmthr/app_apmthr/settings.py
+++ app_apmthr/app_apmthr/settings.py
@@ -14,6 +14,11 @@ from pathlib import Path
 import os
 import dj_database_url
 from datetime import timedelta
+from dotenv import load_dotenv
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,10 +103,9 @@ WSGI_APPLICATION = 'app_apmthr.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': dj_database_url.parse(DATABASE_URL)
 }
+
 
 # DATABASES = {
 #     'default': {
