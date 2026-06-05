56efcbc899284d5e06d1573493019d6fb14fadb0
Manjatiana21 <andrianiainamanjatiana719@gmail.com>
2026-06-05 13:50:26 +0300

    Fix: import useEffect dans App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 // frontend/src/App.js
 import React from "react";
+import React, { useEffect } from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Import de tes composants
