df67600b5027c04a5625ce320981851a649a4698
Manjatiana21 <andrianiainamanjatiana719@gmail.com>
2026-06-10 15:54:41 +0300

    Correction admin.py pour champ image + config Cloudinary

--- app_apmthr/catalogue/admin.py
+++ app_apmthr/catalogue/admin.py
@@ -14,7 +14,8 @@ class FournisseurAdmin(admin.ModelAdmin):
 
 @admin.register(Produit)
 class ProduitAdmin(admin.ModelAdmin):
-    list_display = ('designation', 'stock', 'type_produit', 'fournisseur')
+    list_display = ('designation', 'stock', 'type_produit', 'fournisseur', 'image')
     search_fields = ('designation',)
     list_filter = ('type_produit', 'fournisseur')
+     fields = ('designation', 'description', 'stock', 'type_produit', 'fournisseur', 'prix', 'image', 'is_active')
 
